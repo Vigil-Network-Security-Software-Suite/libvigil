2188389f8aeea2e5350c689ee4b876ccaea9a74c
raging-loon <cxmacolley@gmail.com>
2022-05-25 08:54:01 -0400

    added to libvigil

--- vigil.h
+++ vigil.h
@@ -108,13 +108,28 @@ struct r_raw_ip_data{
 };
 
 
+/* DSIZE PARSING FUNCTIONS AND DEFINITIONS */
+// ops
+#define D_OP_JE               0xa1              // =
+#define D_OP_JL               0xa2              // <
+#define D_OP_JLE              0xa3              // <=
+#define D_OP_JG               0xa4              // >
+#define D_OP_JGE              0xa5              // >=
+#define D_OP_NE               0xa6              // != or !
 
 
-struct spi_member{
-  // fill in
-};
+// parse dsize operators and store the byte code in __rule
+void d_op_parser(struct rule * __rule, const char * str, int type);
 
+// actively apply dsize bytecode from __rule on rdata->pkt_len
+bool d_engine(const struct rule * __rule, const struct rule_data * rdata);
 
+/* RULE PARSING AND INITIALIZATION */
 
+// actually parse the rules
+void line_parser(const char * line);
+
+// initialize rules
+void process_rule(struct rule * r);
 
 #endif /* VIGIL_H */
